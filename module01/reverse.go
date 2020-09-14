6d578a5eba883e98f75dc3e6be486145e752e0ba
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-14 17:01:24 +0100

    string reverse

--- module01/reverse.go
+++ module01/reverse.go
@@ -7,5 +7,9 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	return ""
+	torune := []rune(word)
+	for i, j := 0, len(torune)-1; i < j; i, j = i+1, j-1 {
+		torune[i], torune[j] = torune[j], torune[i]
+	}
+	return string(torune)
 }
