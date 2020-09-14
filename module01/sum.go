d6ffeee674472c32dc996da51a345cab76456e65
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-14 09:57:18 +0100

    sum in go

--- module01/sum.go
+++ module01/sum.go
@@ -2,6 +2,21 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
+// func Sum(numbers []int) int {
+// 	switch reflect.TypeOf(numbers).String() {
+// 	case "[]int":
+// 		var counter int
+// 		for _, x := range numbers {
+// 			counter += x
+// 		}
+// 		return counter
+// 	default:
+// 		return 0
+// 	}
+// }
 func Sum(numbers []int) int {
-	return 0
+	if len(numbers) == 0 {
+		return 0
+	}
+	return numbers[0] + Sum(numbers[1:])
 }
